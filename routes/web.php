87156349f7262e08479d0570aacdb1d79e7d458c
yusufbayuw <yusufbayu31@gmail.com>
2024-01-10 22:42:59 +0700

    cek cf ip

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,23 @@ use App\Http\Controllers\LaporanBulananCabangController;
 */
 
 Route::get('/', function (Request $request) {
-    $userIP = $request->getClientIp();
+    if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+        $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    }
+    $userIP = $_SERVER['REMOTE_ADDR']; //$request->ip();
     $userIParray = explode('.', $userIP);
     if ($userIParray[0] === "10") {
-        return '<p>'.$userIP.'</p>';//view('welcome');
+        return '<p>' . $userIP . '</p>'; //view('welcome');
     } elseif ($userIParray[0] === "172") {
-        return '<p>'.$userIP.'</p>';//view('welcome');
+        return '<p>' . $userIP . '</p>'; //view('welcome');
     } elseif ($userIParray[0] === "192") {
         if ($userIParray[1] === "168") {
-            return '<p>'.$userIP.'</p>';//view('welcome');
+            return '<p>' . $userIP . '</p>'; //view('welcome');
         } else {
-            return '<p>'.$userIP.'</p>';//redirect('/bwi');
+            return '<p>' . $userIP . '</p>'; //redirect('/bwi');
         }
     } else {
-        return '<p>'.$userIP.'</p>';//redirect('/bwi');
+        return '<p>' . $userIP . '</p>'; //redirect('/bwi');
     }
 });
 Route::get('/privacy-policy', function () {
