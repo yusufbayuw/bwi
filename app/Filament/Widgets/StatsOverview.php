653c2c8c167b00f63eda99e71cc2926196dccbe2
Yusuf Bayu Wicaksono <yusuf.wicaksono@tarunabakti.or.id>
2024-05-21 11:13:05 +0700

    perbaikan sort

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -20,7 +20,7 @@ class StatsOverview extends BaseWidget
             $saldoCSR = 0;
             $mutasiAll = Mutasi::all();
             foreach ($cabangs as $key => $cabangid) {
-                $mutasiCabang = $mutasiAll->where('cabang_id', $cabangid)->sortByDesc('created_at')->first();
+                $mutasiCabang = $mutasiAll->where('cabang_id', $cabangid)->orderBy('id', 'DESC')->first();
                 $saldoUmum += (float)($mutasiCabang->saldo_umum);
                 $saldoKeamilan += (float)($mutasiCabang->saldo_keamilan);
                 $saldoCSR += (float)($mutasiCabang->saldo_csr);
