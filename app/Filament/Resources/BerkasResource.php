6be723c9ea426db9a391d08add56f8470e66153d
yusufbayuw <yusufbayu31@gmail.com>
2024-05-22 02:34:17 +0700

    cek

--- app/Filament/Resources/BerkasResource.php
+++ app/Filament/Resources/BerkasResource.php
@@ -49,8 +49,8 @@ class BerkasResource extends Resource
                 Tables\Columns\TextColumn::make('file')
                     ->label('Berkas')
                     ->formatStateUsing(fn ($state) => (explode('.', $state)[1] ?? 'file'))
-                    ->url(fn ($state) => env('APP_URL') . "/storage/" . $state, true)
-                    ->simpleLightbox(),
+                    //->url(fn ($state) => env('APP_URL') . "/storage/" . $state, true)
+                    ->simpleLightbox($state),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
